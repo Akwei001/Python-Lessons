84479aaf42d4d7ab975cd99f21e55f4f976bbbf4
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-08-13 12:22:55 +0100

    string methods

--- LESSON04/data_types.py
+++ LESSON04/data_types.py
@@ -28,3 +28,43 @@ print(decade)
 
 statement = "I like rock music from the " + decade + "s."
 print(statement)
+
+# multiple lines
+multiline = '''
+Hey, how are you?                                                           
+
+I was just checking in.     
+                            All good?
+
+'''
+
+print(multiline)
+
+# Escaping special characters
+
+# /t - tab
+# /n - new line
+# // - backslash,   escaped
+
+sentence = 'I\'m back at work!\tHey!\n\nWhere\'s this at\\located?'
+print(sentence)
+
+# String Methods
+
+print(first)
+print(first.lower())
+print(first.upper())
+print(first)
+
+print(multiline.title())
+print(multiline.replace("good" , "ok"))
+print(multiline)
+
+print(len(multiline))
+multiline += "                                        "
+multiline = "                  " + multiline
+print(len(multiline))
+
+print(len(multiline.strip()))
+print(len(multiline.lstrip()))
+print(len(multiline.rstrip()))
